4724d4645810f4c91d9ab6229c8579f4e579152b
aishah Ali <ayoshali13@gmail.com>
2025-05-11 11:23:59 +0300

    modify lab1.js

--- lab1.js
+++ lab1.js
@@ -1,18 +1,19 @@
 let container = document.getElementById("container");
 
-let api = fetch('https://rickandmortyapi.com/api/character/37')
+let api = fetch('https://rickandmortyapi.com/api/character')
 .then((res)=>res.json())
 .then((data)=>{
     let dataKeys=Object.keys(data)
     dataKeys.find((item) =>{
         let card = document.getElementById("card");
-        let name = document.createElement("p");
+        let Cname = document.createElement("p");
         name.innerText=item.name;
         let img = document.createElement("img");
         img.src=item.image
         img.style.width="30%";
-        card.appendChild(name);
-        card.appendChild(img);
-        container.appendChild(card);
     })
+    card.appendChild(Cname);
+    card.appendChild(img);
+    container.appendChild(card);
+    document.body.appendChild(container);
 })
